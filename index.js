3477205bc36cd9219148824ab143106910be06f5
Sameer Khadka <khadkasameer23@gmail.com>
2023-06-07 16:21:08 +0545

    routes updates

--- index.js
+++ index.js
@@ -8,29 +8,16 @@ app.post("/api/signup", async (req, res) => {
   try {
     let user = await users.create(req.body);
     res.status(200).send(user);
+    console.log(user);
+    bcrypt.hash(req.body.passsword, 10, function (err, hash) {
+      // Store hash in your password DB.
+      // 10 is saltround for hash times in passwords
+    });
   } catch (err) {
     console.log(err.massage);
     if (err.name == "ValidationError") {
       res.status(400).send({
         err: err.massage,
-        // errors: [
-        //   // {
-        //   //   params: "name",
-        //   //   msg: "required",
-        //   // },
-        //   // {
-        //   //   params: "email",
-        //   //   msg: "required",
-        //   // },
-        //   // {
-        //   //   params: "role",
-        //   //   msg: "required",
-        //   // },
-        //   // {
-        //   //   params: "password",
-        //   //   msg: "required",
-        //   // },
-        // ],
       });
     } else {
       res.status(500).send({
@@ -44,7 +31,7 @@ app.post("/api/getusers", async (req, res) => {
   let user = await users.findOne({
     email: req.body.email,
   });
-  res.send(user.password11);
+  res.send(user.password);
 });
 
 app.listen(8080, () => {
